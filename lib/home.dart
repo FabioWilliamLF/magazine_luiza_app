102a34fdc3ab17308265941bf271350a1151f660
FabioWilliamLF <fabiowilliamlf@gmail.com>
2021-08-18 07:48:09 -0300

    adição de todos arquivos

---
+++ lib/home.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'bank/products.dart';
+import 'package:layout_basico/models/prod.dart';
+import 'style.dart';
+
+class Home extends StatefulWidget {
+  @override
+  createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Ofertas do dia",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: Padding(
+          padding: EdgeInsets.all(8),
+          child: Image.asset("assets/images/logo.jpg"),
+        ),
+        backgroundColor: blueTheme,
+      ),
+      body: Column(
+        children: [
+          Image.asset("assets/images/linha.jpg"),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: builder,
+              itemCount: listProducts.length,
+              separatorBuilder: (BuildContext _, int index) {
+                return SizedBox(
+                  height: 6,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget builder(BuildContext _, int index) {
+    Products _products = listProducts.elementAt(index);
+    return Column(
+      children: [
+        ListTile(
+          leading: Image.asset(_products.photo),
+          trailing: IconButton(
+            icon: (_products.isFavorite)
+                ? Icon(
+                    Icons.favorite,
+                    color: pinkTheme,
+                  )
+                : Icon(
+                    Icons.favorite_outline,
+                    color: pinkTheme,
+                  ),
+            onPressed: () {
+              setState(
+                () {
+                  _products.isFavorite = !_products.isFavorite;
+                },
+              );
+            },
+          ),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _products.name,
+                style: TextStyle(
+                  fontSize: 13,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                _products.description,
+                style: TextStyle(
+                  fontSize: 10,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                _products.value,
+                style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                  color: blueTheme,
+                ),
+              ),
+              Text(
+                _products.installments,
+                style: TextStyle(
+                  fontSize: 10,
+                  color: blueTheme,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color pinkTheme = Color(0xFFE147B5);
+
+MaterialColor blueTheme = MaterialColor(
+  0xFF0892FF,
+  {
+    50: Color(0xFFF3FAFF),
+    100: Color(0xFFE7F5FF),
+    200: Color(0xFFC2E4FF),
+    300: Color(0xFF9AD3FF),
+    400: Color(0xFF53B3FF),
+    500: Color(0xFF0892FF),
+    600: Color(0xFF0882E3),
+    700: Color(0xFF055899),
+    800: Color(0xFF044273),
+    900: Color(0xFF032B4A),
+  },
+);
